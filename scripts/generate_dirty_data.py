f7ebaaa4300c1bb6513e1beaa41ef9d5bf5389ac
Austin-Nakana <austinnakana@gmail.com>
2026-06-12 20:03:13 +0200

    update mispellings

--- scripts/generate_dirty_data.py
+++ scripts/generate_dirty_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 from datetime import datetime, timedelta
 
 
+#schools, districts, campaigns, and platforms
 schools = ["Brixton High", "Sandton College", "Soweto Academy",
            "Midrand High", "Alexandra School", "Rosebank Academy",
            "Randburg High"]
